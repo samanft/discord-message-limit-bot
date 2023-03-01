848b766b7c888934fd774e28768845b7752ea7d6
samanft <poopeekaska@gmail.com>
2023-03-01 14:05:29 +0100

    test

--- index.js
+++ index.js
@@ -24,41 +24,22 @@ client.on('message', async message => {
 
     let messageCount = 0;
 
-    guild.channels.cache.each(async channel => {
-        if (channel.type === 'text') {
-            let messages = await channel.messages.fetch({ limit: 100 });
-            let lastMessageId;
-            await messages.each(async message => {
-                if (message.author.id === '863832130730721280' && isToday(message.createdTimestamp)) {
-                    messageCount++;
-                    console.log(`Shark has sent ${messageCount} messages today.`);
+    client.on('message', async message => {
+        const guild = message.guild;
+        if (!guild) return;
 
-                    if (messageCount >= 50) {
-                        const user = guild.members.cache.get('863832130730721280'); // replace '1234567890' with the ID of the user you want to mute
-                        user.roles.add(muteRoleId).then(() => {
-                            const remainingTime = getRemainingTime();
-                            const remainingHours = Math.floor(remainingTime / (60 * 60 * 1000));
-                            const remainingMinutes = Math.floor((remainingTime / (60 * 1000)) % 60);
-                            console.log(`Muted ${user.user.tag} for exceeding message limit. Unmuting ${user.user.tag} in ${remainingHours} hours and ${remainingMinutes} minutes.`);
-                            setTimeout(() => {
-                                getRemainingTime();
-                                user.roles.remove(muteRoleId).then(() => {
-                                    console.log(`Unmuted ${user.user.tag}.`);
-                                });
-                            }, getDelayUntilEndOfDay() / 1000);
-                        });
-                    }
-                }
-                lastMessageId = message.id;
-            });
-            while (messages.size === 100) {
-                messages = await channel.messages.fetch({ limit: 100, before: lastMessageId });
+        let messageCount = 0;
+
+        guild.channels.cache.each(async channel => {
+            if (channel.type === 'text') {
+                let messages = await channel.messages.fetch({ limit: 100 });
+                let lastMessageId;
                 await messages.each(async message => {
                     if (message.author.id === '863832130730721280' && isToday(message.createdTimestamp)) {
                         messageCount++;
                         console.log(`Shark has sent ${messageCount} messages today.`);
 
-                        if (messageCount >= 40) {
+                        if (messageCount >= 50) {
                             const user = guild.members.cache.get('863832130730721280');
                             user.roles.add(muteRoleId).then(() => {
                                 const remainingTime = getRemainingTime();
@@ -66,18 +47,42 @@ client.on('message', async message => {
                                 const remainingMinutes = Math.floor((remainingTime / (60 * 1000)) % 60);
                                 console.log(`Muted ${user.user.tag} for exceeding message limit. Unmuting ${user.user.tag} in ${remainingHours} hours and ${remainingMinutes} minutes.`);
                                 setTimeout(() => {
-                                    getRemainingTime();
                                     user.roles.remove(muteRoleId).then(() => {
                                         console.log(`Unmuted ${user.user.tag}.`);
                                     });
-                                }, getDelayUntilEndOfDay() / 1000);
+                                }, remainingTime);
                             });
                         }
                     }
                     lastMessageId = message.id;
                 });
+                while (messages.size === 100) {
+                    messages = await channel.messages.fetch({ limit: 100, before: lastMessageId });
+                    await messages.each(async message => {
+                        if (message.author.id === '863832130730721280' && isToday(message.createdTimestamp)) {
+                            messageCount++;
+                            console.log(`Shark has sent ${messageCount} messages today.`);
+
+                            if (messageCount >= 40) {
+                                const user = guild.members.cache.get('863832130730721280');
+                                user.roles.add(muteRoleId).then(() => {
+                                    const remainingTime = getRemainingTime();
+                                    const remainingHours = Math.floor(remainingTime / (60 * 60 * 1000));
+                                    const remainingMinutes = Math.floor((remainingTime / (60 * 1000)) % 60);
+                                    console.log(`Muted ${user.user.tag} for exceeding message limit. Unmuting ${user.user.tag} in ${remainingHours} hours and ${remainingMinutes} minutes.`);
+                                    setTimeout(() => {
+                                        user.roles.remove(muteRoleId).then(() => {
+                                            console.log(`Unmuted ${user.user.tag}.`);
+                                        });
+                                    }, remainingTime);
+                                });
+                            }
+                        }
+                        lastMessageId = message.id;
+                    });
+                }
             }
-        }
+        });
     });
 });
 
