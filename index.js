4cdaa484865bf456d750ff26626cce3d08b88989
samanft <poopeekaska@gmail.com>
2023-03-02 09:05:25 +0100

    fixed muting issue

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ client.setMaxListeners(20);
 
 console.log(process.env);
 
+let messageCount = 0;
+let countingMessages = false;
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
@@ -25,41 +28,17 @@ client.on('message', async message => {
 
     if (message.author.id !== '863832130730721280' && message.author.id !== '290084627522125824') return;
 
-    let messageCount = 0;
-
-    guild.channels.cache.each(async channel => {
-        if (channel.type === 'text') {
-            let messages = await channel.messages.fetch({ limit: 100 });
-            let lastMessageId;
-            await messages.each(async message => {
-                if (message.author.id === '863832130730721280' && isToday(message.createdTimestamp)) {
-                    messageCount++;
-                    console.log(`Shark has sent ${messageCount} messages today.`);
-
-                    if (messageCount >= 50) {
-                        const user = guild.members.cache.get('863832130730721280');
-                        user.roles.add(muteRoleId).then(() => {
-                            const remainingTime = getRemainingTime();
-                            const remainingHours = Math.floor(remainingTime / (60 * 60 * 1000));
-                            const remainingMinutes = Math.floor((remainingTime / (60 * 1000)) % 60);
-                            console.log(`Muted ${user.user.tag} for exceeding message limit. Unmuting ${user.user.tag} in ${remainingHours} hours and ${remainingMinutes} minutes.`);
-                            setTimeout(() => {
-                                user.roles.remove(muteRoleId).then(() => {
-                                    console.log(`Unmuted ${user.user.tag}.`);
-                                });
-                            }, remainingTime);
-                        });
-                    }
-                }
-                lastMessageId = message.id;
-            });
-            while (messages.size === 100) {
-                messages = await channel.messages.fetch({ limit: 100, before: lastMessageId });
+    if (!countingMessages) {
+        countingMessages = true;
+        messageCount = 0;
+        guild.channels.cache.each(async channel => {
+            if (channel.type === 'text') {
+                let messages = await channel.messages.fetch({ limit: 100 });
+                let lastMessageId;
                 await messages.each(async message => {
                     if (message.author.id === '863832130730721280' && isToday(message.createdTimestamp)) {
                         messageCount++;
                         console.log(`Shark has sent ${messageCount} messages today.`);
-
                         if (messageCount >= 50) {
                             const user = guild.members.cache.get('863832130730721280');
                             user.roles.add(muteRoleId).then(() => {
@@ -77,9 +56,34 @@ client.on('message', async message => {
                     }
                     lastMessageId = message.id;
                 });
+                while (messages.size === 100) {
+                    messages = await channel.messages.fetch({ limit: 100, before: lastMessageId });
+                    await messages.each(async message => {
+                        if (message.author.id === '863832130730721280' && isToday(message.createdTimestamp)) {
+                            messageCount++;
+                            console.log(`Shark has sent ${messageCount} messages today.`);
+                            if (messageCount >= 50) {
+                                const user = guild.members.cache.get('863832130730721280');
+                                user.roles.add(muteRoleId).then(() => {
+                                    const remainingTime = getRemainingTime();
+                                    const remainingHours = Math.floor(remainingTime / (60 * 60 * 1000));
+                                    const remainingMinutes = Math.floor((remainingTime / (60 * 1000)) % 60);
+                                    console.log(`Muted ${user.user.tag} for exceeding message limit. Unmuting ${user.user.tag} in ${remainingHours} hours and ${remainingMinutes} minutes.`);
+                                    setTimeout(() => {
+                                        user.roles.remove(muteRoleId).then(() => {
+                                            console.log(`Unmuted ${user.user.tag}.`);
+                                        });
+                                    }, remainingTime);
+                                });
+                            }
+                        }
+                        lastMessageId = message.id;
+                    });
+                }
             }
-        }
-    });
+        });
+        countingMessages = false;
+    }
 });
 
 function isToday(timestamp) {
@@ -87,12 +91,6 @@ function isToday(timestamp) {
     const today = now.startOf('day');
     const messageDate = DateTime.fromMillis(timestamp).setZone('Asia/Singapore').startOf('day');
     return today.equals(messageDate);
-}
-
-function getDelayUntilEndOfDay() {
-    const now = DateTime.now().setZone('Asia/Singapore');
-    const endOfDay = now.endOf('day');
-    return endOfDay.diff(now).as('milliseconds');
 }
 
 function getRemainingTime() {
